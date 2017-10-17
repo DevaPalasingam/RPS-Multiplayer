7fbf084ad0cba7b45328611461972a2b15f5a933
Deva <deva.palasingam@gmail.com>
2017-10-17 17:17:58 -0500

    first commit

---
+++ assets/javascript/app.js
@@ -0,0 +1,12 @@
+// Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyC4AVnj91UhmFvRCS41hcSKWDuzbETa1JY",
+    authDomain: "rps-multiplayer-59265.firebaseapp.com",
+    databaseURL: "https://rps-multiplayer-59265.firebaseio.com",
+    projectId: "rps-multiplayer-59265",
+    storageBucket: "",
+    messagingSenderId: "584404982981"
+  };
+  firebase.initializeApp(config);
+
+  
