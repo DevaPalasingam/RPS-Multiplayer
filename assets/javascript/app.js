0ce1abe6ea1b3c993dfaf0a68a116f64a2fe9617
Deva <deva.palasingam@gmail.com>
2017-10-17 23:01:44 -0500

    long long long way to go

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -18,4 +18,38 @@
   	this.win = win;
   }
 
-  var playerNumber = 0;
+  var peopleOnline;
+  var peoplePlaying;
+
+  //Document ready: waits for the html to load before running other code
+  $ (document).ready(function(){
+
+  	//People online: this code will count how many people are online and then display it
+  	var connectionsRef = dataRef.ref("/connections");
+  	var connectedRef = dataRef.ref(".info/connected");
+  	connectedRef.on("value", function(snap) {
+  		 if (snap.val()) {
+  		 	var con = connectionsRef.push(true);
+  		 	con.onDisconnect().remove();
+  		 }
+  	});
+
+  	connectionsRef.on("value", function(snap) {
+  		$("#watchers").text(snap.numChildren());
+  		peopleOnline = snap.numChildren();
+  		console.log("people online: " + peopleOnline);
+  	});
+  	//People online:=========================================
+
+
+  	//Enter key: this code runs when the enter key is pressed
+  	$(document).keypress(function(e) {
+  		if(e.which == 13) {
+  			console.log("you pushed enter")
+
+  		}
+  	});
+  	//Enter key:=============================================
+
+  });
+  //Document ready:==========================================
